a3f45ab1c0e0389053f28bbdd2d57d2643b611d9
啊o额iu鱼 <aoeiuv020@gmail.com>
2021-09-17 12:12:19 +0800

    修复环境变量可能无效的问题
    不该用与或代替判断，环境变量中的配置等于假时会强制使用默认配置，

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 // 项目名，决定html从哪个项目获取，
-const github_repo = (typeof(GITHUB_REPO)!="undefined"&&GITHUB_REPO)
-    ||'AoEiuV020/Url-Shorten-Worker'
+const github_repo = typeof(GITHUB_REPO)!="undefined" ? GITHUB_REPO
+    : 'AoEiuV020/Url-Shorten-Worker'
 // 项目版本，cdn会有缓存，所以有更新时需要指定版本，
-const github_version = (typeof(GITHUB_VERSION)!="undefined"&&GITHUB_VERSION)
-    ||'@main'
+const github_version = typeof(GITHUB_VERSION)!="undefined" ? GITHUB_VERSION
+    : '@main'
 // 密码，密码正确情况无视白名单和超时设置，且支持自定义短链接，
-const password = (typeof(PASSWORD)!="undefined"&&PASSWORD)
-    ||'AoEiuV020 yes'
+const password = typeof(PASSWORD)!="undefined" ? PASSWORD
+    : 'AoEiuV020 yes'
 // 短链超时，单位毫秒，支持整数乘法，0表示不设置超时，
-const shorten_timeout = (typeof(SHORTEN_TIMEOUT)!="undefined"&&SHORTEN_TIMEOUT.split("*").reduce((a,b)=>parseInt(a)*parseInt(b),1))
-    ||(1000 * 60 * 10)
+const shorten_timeout = typeof(SHORTEN_TIMEOUT)!="undefined" ? SHORTEN_TIMEOUT.split("*").reduce((a,b)=>parseInt(a)*parseInt(b),1)
+    : (1000 * 60 * 10)
 // 默认短链key的长度，遇到重复时会自动延长，
-const default_len = (typeof(DEFAULT_LEN)!="undefined"&&parseInt(DEFAULT_LEN))
-    || 6
+const default_len = typeof(DEFAULT_LEN)!="undefined" ? parseInt(DEFAULT_LEN)
+    : 6
 // 为true开启演示，否则无密码且非白名单请求不受理，是则允许访客试用，超时后失效，
-const demo_mode = (typeof(DEMO_MODE)!="undefined"&&DEMO_MODE === 'true')
-    ||true
+const demo_mode = typeof(DEMO_MODE)!="undefined" ? DEMO_MODE === 'true'
+    : true
 // 白名单中的域名无视超时，json数组格式，写顶级域名就可以，自动通过顶级域名和所有二级域名，
-const white_list = JSON.parse((typeof(WHITE_LIST)!="undefined"&&WHITE_LIST)
-    ||`[
+const white_list = JSON.parse(typeof(WHITE_LIST)!="undefined" ? WHITE_LIST
+    : `[
 "aoeiuv020.com",
 "aoeiuv020.cn",
 "aoeiuv020.cc",
 "020.name"
     ]`)
 // 演示模式开启时网页上展示这段禁止滥用提示，并不需要明确表示什么时候失效，
-const demo_notice = (typeof(DEMO_NOTICE)!="undefined"&&DEMO_NOTICE)
-    ||`注意：为防止示例服务被人滥用，故所有由demo网站生成的链接随时可能失效，如需长期使用请自行搭建。`
+const demo_notice = typeof(DEMO_NOTICE)!="undefined" ? DEMO_NOTICE
+    : `注意：为防止示例服务被人滥用，故所有由demo网站生成的链接随时可能失效，如需长期使用请自行搭建。`
 //console.log(`${github_repo}, ${github_version}, ${password}, ${shorten_timeout}, ${demo_mode}, ${white_list}, ${demo_notice}`)
 const html404 = `<!DOCTYPE html>
 <body>
